3df4c3be92d41868db3d444329789f4338a4617d
MichaelRaatsPro <michaelraatspro@gmail.com>
2024-03-19 15:27:57 +0200

    Made changed to default react app

--- Lab Project/my-react-app/src/index.js
+++ Lab Project/my-react-app/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('sandy')).render(<p>Hello</p>);
+
+//const myElement = <h1>React is {5 + 5} times better with JSX</h1>;
+
+const myElement = (
+  <>
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(myElement);
+//root.render(myFirstElement); //renders html page
+
+//If we have to write HTML on multiple lines, we can put the HTML in parentheses:
