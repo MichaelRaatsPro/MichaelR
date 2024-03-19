ddaf691f0468af8842eabd5667f585a36245e0e4
MichaelRaatsPro <michaelraatspro@gmail.com>
2024-03-19 15:45:52 +0200

    Progessed in alison react project

--- Lab Project/my-react-app/src/index.js
+++ Lab Project/my-react-app/src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 
 ReactDOM.createRoot(document.getElementById('sandy')).render(<p>Hello</p>);
 
 //const myElement = <h1>React is {5 + 5} times better with JSX</h1>;
+
+function MyForm() {                                 //practice
+  const [textarea, setTextarea] = useState(          
+    "Enter text here..."
+  );
+  
+  const handleChange = (event) => {                                     //event to setTextArea
+    setTextarea(event.target.value) 
+  }
+
+  return (                      
+    <form> 
+      <textarea value = {textarea} onChange = {handleChange} />        
+    </form>                                                         //change value in the textarea
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyForm />);
+
+
+
+
+
 
 const myElement = (
   <>
@@ -14,7 +39,7 @@ const myElement = (
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(myElement);
+//ReactDOM.createRoot(document.getElementById('root')).render(myElement);
 //root.render(myFirstElement); //renders html page
 
 //If we have to write HTML on multiple lines, we can put the HTML in parentheses:
